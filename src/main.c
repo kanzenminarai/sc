6b4bd38e725be619e4919fcf4acf33cf20204428
kanzen minarai <kanzenminarai@proton.me>
2024-05-22 18:35:15 -0400

    Changed the source code structure, now segmented in more files

--- src/main.c
+++ src/main.c
@@ -1,46 +1,19 @@
 #include <stdio.h>
+#include "../include/evalI64.h"
 
-// cannot do multiple calculations in a single line, and can't do floating point calculations
-int testing(char *source) {
-  int number[2], result = 0;
-  char operator = 0;
-  unsigned int counter = 0, index = 0;
-  while(source[counter] != '\0') {
-    if(source[counter] == '+' || source[counter] == '-' || source[counter] == '*' || source[counter] == '/') {
-      if(index == 1) index = 0;
-      else index = 1;
-      operator = source[counter];
-    }
-    if(source[counter] >= '0' && source[counter] <= '9')
-      number[index] = (number[index] * 10) + (source[counter] - '0');
-    counter++;
-  }
-  switch(operator) {
-    case '+':
-      result = number[0] + number[1];
-      break;
-
-    case '-':
-      result = number[0] - number[1];
-      break;
-
-    case '*':
-      result = number[0] * number[1];
-      break;
-
-    case '/':
-      result = number[0] / number[1];
-      break;
-
-    default:
-      result = 0;
-      break;
-  }
-  return result;
+// Initialize all the elements in "i64 stackI64" array with the value 0
+static inline void initializeStack(StackI64 *stack) {
+  for(u8 i = 0; i < MAX_INDEX; i++) stack->stackI64[i] = 0;
 }
 
-int main(int argc, char **argv) {
-  char *str = argv[argc - 1];
-  printf("%i\n", testing(str));
+int main() {
+  char expression[100];
+  StackI64 st;
+
+  initializeStack(&st);
+
+  fgets(expression, sizeof(expression), stdin);
+  printf("output:%li\n\n", evalPostFixI64(&st, expression));
+
   return 0;
 }
