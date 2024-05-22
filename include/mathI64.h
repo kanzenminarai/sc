f00a2ad711dc5afdf005e9728b599e1b064a0788
kanzen minarai <kanzenminarai@proton.me>
2024-05-22 18:51:21 -0400

    Added the math functions prototype

---
+++ include/mathI64.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "../include/types.h"
+
+// Calculates factorial using "const i64 value" and returns a 64 bit long integer value
+i64 factorialI64(const i64 value);
+
+// Calculates power using "const i64 number" and "const i64 expoent" and returns a 64 bit long integer value
+i64 powerI64(const i64 number, const i64 expoent);
