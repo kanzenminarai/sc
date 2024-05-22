fa8494539a7377d8bcfe07b49107efd1007b35e4
kanzen minarai <kanzenminarai@proton.me>
2024-05-22 18:40:30 -0400

    Added new names to abbreviate types

---
+++ include/types.h
@@ -0,0 +1,15 @@
+#pragma once
+
+// Integer types
+typedef signed long int i64;
+typedef signed char i8;
+typedef unsigned short int u16;
+typedef unsigned char u8;
+
+// Boolean constants
+typedef unsigned char boolean;
+#define TRUE 1
+#define FALSE 0
+
+// Float type
+typedef double f64;
